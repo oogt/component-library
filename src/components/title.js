11809e76d0696d10d8ec44ad6089eea522996aa3
Robbert van Caem <robbertvancaem@gmail.com>
2019-05-21 13:45:43 +0200

    Add delay prop to Title component

--- src/components/title.js
+++ src/components/title.js
@@ -28,9 +28,10 @@ const Overlay = styled.div`
   top: 0;
   left: 0;
   right: 0;
-  transition: all ${({ duration }) => duration || DURATION}ms ${medium};
+  transition: all ${({ duration }) => duration}ms ${medium};
   animation-name: ${move};
-  animation-duration: ${({ duration }) => (duration || DURATION) * 2}ms;
+  animation-delay: ${({delay}) => delay}ms;
+  animation-duration: ${({ duration }) => duration * 2}ms;
   animation-fill-mode: both;
   transform: scaleX(0);
   transform-origin: left;
@@ -40,26 +41,30 @@ const H1 = styled.h1`
   margin: 0;
   opacity: 0;
   animation-name: ${fadeIn};
-  animation-delay: ${({ duration }) => duration || DURATION}ms;
-  animation-duration: ${({ duration }) => (duration || DURATION) / 2}ms;
+  animation-delay: ${({ duration, delay }) => duration + delay}ms;
+  animation-duration: ${({ duration }) => duration / 2}ms;
   animation-fill-mode: both;
   color: ${({ textColor }) => textColor || defaultTextColor};
   font-size: 4em;
 `;
 
-const Title = ({ duration, color, textColor, children }) => (
+const Title = ({ duration, color, textColor, children, delay }) => (
   <LazyLoad>
     <div>
-      {children.map((line, index) => (
-        <Wrapper key={index}>
-          <LineWrapper>
-            <H1 textColor={textColor} duration={duration}>
-              {line}
-            </H1>
-            <Overlay color={color} duration={duration} />
-          </LineWrapper>
-        </Wrapper>
-      ))}
+      {children.map((line, index) => {
+        const actualDuration = duration === undefined ? DURATION : duration;
+        const actualDelay = (delay === undefined ? DURATION : delay) * index ;
+        return (
+          <Wrapper key={index}>
+            <LineWrapper>
+              <H1 textColor={textColor} duration={actualDuration} delay={actualDelay}>
+                {line}
+              </H1>
+              <Overlay color={color} duration={actualDuration} delay={actualDelay} />
+            </LineWrapper>
+          </Wrapper>
+        );
+      })}
     </div>
   </LazyLoad>
 );
--- src/stories/title.js
+++ src/stories/title.js
@@ -1,12 +1,9 @@
 import React from "react";
-import { storiesOf} from "@storybook/react";
+import { storiesOf } from "@storybook/react";
 import { button, color, array, number } from "@storybook/addon-knobs";
 
-import {
-  defaultColor,
-  defaultTextColorDark,
-} from "../config/colors";
-import { counter, reRender } from './index';
+import { defaultColor, defaultTextColorDark } from "../config/colors";
+import { counter, reRender } from "./index";
 import Section from "../components/section";
 import Title from "../components/title";
 
@@ -21,11 +18,17 @@ storiesOf("Titles", module).add("with reveal effect", () => {
         return (
           <Section even={isEven} key={titleCount}>
             <Title
-              duration={number('Duration (ms)', 500, {
+              duration={number("Duration (ms)", 500, {
                 range: true,
                 min: 250,
                 max: 5000,
                 step: 250
+              })}
+              delay={number("Delay (ms)", 500, {
+                range: true,
+                min: 0,
+                max: 2000,
+                step: 100
               })}
               color={color("Block color", defaultColor)}
               textColor={color("Text color", defaultTextColorDark)}
